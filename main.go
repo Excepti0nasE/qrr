149440ecb69e3503da558e696d8e4227d32ba3ab
Exception <hexception@naver.com>
2019-12-13 20:02:27 +0800

    without learning go flags, little test

--- main.go
+++ main.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"github.com/tuotoo/qrcode"
+	"os"
 )
+
+const appname = "qrr"
+const version = "0.0.1"
+const platform = "darwin/amd64"
 
 func main() {
 	s := os.Args[1]
-	fi, err := os.Open(s)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	switch s {
+	case "--version":
+		fmt.Printf("%v verion %v %v\n", appname, version, platform)
+	case "--help":
+		fmt.Println("Just show some help infos")
+	default:
+		fi, err := os.Open(s)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer fi.Close()
+		qrmatrix, err := qrcode.Decode(fi)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(qrmatrix.Content)
 	}
-	defer fi.Close()
-	qrmatrix, err := qrcode.Decode(fi)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(qrmatrix.Content)
 }
